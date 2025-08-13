c17813909c9ceaa11e439da21b578c63b265256b
Levion6611 <tony6611brown@gmail.com>
2025-08-13 10:25:46 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -90,7 +90,6 @@ const userSchema = new mongoose.Schema({
   marketorName: { type: String, default: ''},
   businessName: { type: String, default: ''},
   profileBusiness: { type: Number, default: 0},
-  normalizedPhone: { type: String, unique: true, sparse: true },
 }, {
   timestamps: true // Active createdAt et updatedAt automatiquement
 });
